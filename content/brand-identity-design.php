26c5dd5a05a02a21fef7c0628ba515ee573f9427
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-06-29 23:44:58 +0200

    Polykit Migration

---
+++ content/brand-identity-design.php
@@ -0,0 +1,13 @@
+<h2 id="brand-identity-design">Brand Identity Design</h2>
+<img loading="lazy" src="imgs/brand-identity-design-chart.webp">
+<p>The goal of a Brand Identity Design project is to <strong>translate the Business Strategy into a distinctive trademark</strong> which reflects the business goals and aspirations while appealing to the target public.</p>
+<img loading="lazy" src="imgs/business-value.webp">
+<p>The Business Development process aims to increase the value of the business' products and services, while the aim of the Brand Identity Design is to <strong>reflect the Quality Value and increase the Perceived Value</strong> in order to attract potential customers.</p>
+
+<?php include 'content/brand-strategy.php'; ?>
+<hr class="secondary-separator">
+<?php include 'content/visual-identity.php'; ?>
+<hr class="secondary-separator">
+<?php include 'content/brand-guidelines.php'; ?>
+<hr class="secondary-separator">
+<?php include 'content/brand-touchpoints.php'; ?>
--- index.php
+++ index.php
@@ -5,76 +5,121 @@
   <?php include 'sections/head.php';?>
 <!-- INCLUDE HEAD.PHP end --->
 
-  <body id="getting-started-container" class="light-theme">
+  <body class="light-mode docs-theme polyatlas-site">
 
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKN32JZ"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
-    <!-- INCLUDE HEADER.PHP start -->
-      <?php include 'sections/header.php';?>
-    <!-- INCLUDE HEADER.PHP end -->
-
-    <!-- INCLUDE SIDEBAR.PHP start -->
-      <?php include 'sections/sidebar.php';?>
-    <!-- INCLUDE SIDEBAR.PHP end -->
-
-    <!-- INCLUDE HEADER.PHP start -->
-      <?php include 'sections/version-banner.php';?>
-      <?php include 'sections/about-banner.php';?>
-      <?php include 'sections/poly-forum-banner.php';?>
-      <?php include 'sections/donations.php';?>
-    <!-- INCLUDE HEADER.PHP end -->
-
-    <div class="container-fluid">
-      <div class="row">
-        <div id="content" style="width:100%; max-width: 750px;">
-
-        <div id="poly-atlas-heading">
-          <a href="../"><img id="poly-atlas-logo" src="imgs/poly-atlas.svg" style="background:none !important;box-shadow:none;height:50px;width:auto;"><img id="poly-atlas-negative-logo" src="imgs/poly-atlas-negative.svg" style="background:none !important;box-shadow:none;height:50px;width:auto;"></a>
-          <a id="github-link" href="https://github.com/polyfen/polyatlas" target="_blank" style="float: right;color: #FFFFFF88;"><i class="fa-brands fa-github" style="margin-right: 2px;"></i> an open-source project</a>
-          <a id="slack-link" href="https://polyforum.org/join" target="_blank" style="float: right;color: #FFFFFF88;"><i class="fa-brands fa-slack" style="margin-right: 2px;"></i> join our community on Slack</a>
-        </div>
-        <hr class="second-separator first-separator">
-
-        <h1>A Guide of Best Practices in Branding</h1>
-        <p>Poly Atlas features a step-by-step guide to the Branding process including practical tips, case studies, template files, and many other useful resources.</p>
-        
-        <hr class="second-separator">
-
-        <h2 id="getting-started">Getting Started</h2>
-          <p>This framework can be a valuable resource for any person with an interest in Branding, whether you are a seasoned creative professional, a rookie design or marketing student, an entrepreneur or business owner, or simply a Branding enthusiast.</p>
-          <p>Each section of <strong>this platform breaks down the Branding process into individual goals</strong> along with: (1) a general description of each goal, (2) an account of whom in the Branding team should be appointed to perform each task, (3) a rough estimation of the hours of work allocated to each goal, and (4) further information, resources, and case studies.</p>
-          <p>Branding is a multidisciplinary process and hence it's best performed by a team of professionals who specialise on each discipline. However, whether you wish to design Brand Identities for clients as a part of a creative team or as a solo freelancer, following this framework —and, if necessary, adapting it to your specific needs— aims to <strong>(1) provide you with a better understanding of the Branding process,</strong> and <strong>(2) optimising and expediting your workflow,</strong> allowing you to fast-track all the minor administrative tasks and allowing you to focus your efforts on the creative output.</p>
-          <p>All along this workflow you will find entries titled "The Polyfen Group" which showcase how we, at The Polyfen Group, have performed each task.</p>
-          <p>However, bear in mind that <strong>every Branding or Brand Identity Design project is unique and can be vastly different</strong> depending on the specifics of the business, industry, budget, time frame, target public, and the skill level of the people involved, as well as their resources and preferred work methodology.</p>
-          <p>All authored content within this document is distributed for free by <a href="http://mynameisboris.com" target="_blank">Boris Hrncic</a> &amp; <a href="http://thepolygroup.co" target="
-            ">The Polyfen Group</a> under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</p>
-          <p>Furthermore, this is a collection of what we at <a href="http://thepolygroup.co" target="_blank">The Polyfen Group</a> consider to be the best practices in Branding, but other authors may digress.</p>
-          <p>You can be a part of the conversation and share your comments and questions in <strong>our online community</strong> <a href="http://polyforum.org" target="_blank">Poly Forum</a>, a place to discuss branding, share tips and resources, ask questions, request feedback, and connect with other creatives around the world.</p>
-          <p>Now, let's go ahead and get started!</p>
-
-        <hr>
-
-        <div class="inter-page-navigation">
-          <a class="next-page-title" href="intro-to-branding.php">
-            <small>Next ›</small>
-            <small>Intro to Branding</small>
+    <div class="main-wrapper">
+      <aside>
+          <a href="/" class="logo">
+              <img src="imgs/poly-atlas-logo.svg" alt="Poly Atlas" height="40px" width="auto">
           </a>
-          <div style="clear:both;"></div>
-        </div>
+            <h2 class="caption uppercase">Table of Contents</h2>
+            <ul class="table-of-contents">
+            <li><a href="#getting-started" id="toc-getting-started" class="caption">Getting Started</a></li>
+            <li><a href="#intro-to-branding" id="toc-intro-to-branding" class="caption">Intro to Branding</a></li>
+            <ul>
+            <li><a href="#what-is-a-brand" id="toc-what-is-a-brand" class="caption">What is a Brand?</a></li>
+            <li><a href="#what-is-branding" id="toc-what-is-branding" class="caption">What is Branding?</a></li>
+            <li><a href="#what-is-a-logo" id="toc-what-is-a-logo" class="caption">What is a Logo?</a></li>
+            <li><a href="#terminology" id="toc-terminology" class="caption">Terminology</a></li>
+            </ul>
+            <li><a href="#business-development" id="toc-business-development" class="caption">Business Development</a></li>
+            <ul>
+            <li><a href="#market-research" id="toc-market-research" class="caption">Market Research</a></li>
+            <ul>
+            <li><a href="#business-analysis" id="toc-business-analysis" class="caption">Business analysis</a></li>
+            <li><a href="#target-public-analysis" id="toc-target-public-analysis" class="caption">Target public analysis</a></li>
+            <li><a href="#industry-analysis" id="toc-industry-analysis" class="caption">Industry analysis</a></li>
+            <li><a href="#research-conclusion" id="toc-research-conclusion" class="caption">Research conclusion</a></li>
+            </ul>
+            <li><a href="#business-plan" id="toc-business-plan" class="caption">Business Plan</a></li>
+            <li><a href="#products-and-services" id="toc-products-and-services" class="caption">Products &amp; Services</a></li>
+            </ul>
+            <li><a href="#brand-identity-design" id="toc-brand-identity-design" class="caption">Brand Identity Design</a></li>
+            <ul>
+            <li><a href="#brand-strategy" id="toc-brand-strategy" class="caption">Brand Strategy</a></li>
+            <ul>
+            <li><a href="#voice" id="toc-voice" class="caption">Voice</a></li>
+            <li><a href="#tone" id="toc-tone" class="caption">Tone</a></li>
+            <li><a href="#keywords" id="toc-keywords" class="caption">Keywords</a></li>
+            <li><a href="#creative-concept" id="toc-creative-concept" class="caption">Creative concept</a></li>
+            <li><a href="#moodboard" id="toc-moodboard" class="caption">Moodboard</a></li>
+            <li><a href="#naming" id="toc-naming" class="caption">Naming</a></li>
+            <li><a href="#brand-architecture" id="toc-brand-architecture" class="caption">Brand architecture</a></li>
+            <li><a href="#key-messages" id="toc-key-messages" class="caption">Key messages</a></li>
+            </ul>
+            <li><a href="#visual-identity" id="toc-visual-identity" class="caption">Visual Identity</a></li>
+            <ul>
+            <li><a href="#visual-exploration" id="toc-visual-exploration" class="caption">Visual exploration</a></li>
+            <li><a href="#logo-system" id="toc-logo-system" class="caption">Logo system</a></li>
+            <li><a href="#typeface-selection" id="toc-typeface-selection" class="caption">Typeface selection</a></li>
+            <li><a href="#colour-scheme" id="toc-colour-scheme" class="caption">Colour scheme</a></li>
+            <li><a href="#visual-language" id="toc-visual-language" class="caption">Visual language</a></li>
+            </ul>
+            <li><a href="#brand-guidelines" id="toc-brand-guidelines" class="caption">Brand Guidelines</a></li>
+            <li><a href="#brand-touchpoints" id="toc-basic-touchpoints" class="caption">Brand Touchpoints</a></li>
+            <ul>
+            <li><a href="#social-media" id="toc-social-media" class="caption">Social media</a></li>
+            <li><a href="#parking-page" id="toc-parking-page" class="caption">Parking page</a></li>
+            <li><a href="#email-signature" id="toc-email-signature" class="caption">Email signature</a></li>
+            <li><a href="#business-cards" id="toc-business-cards" class="caption">Business cards</a></li>
+            <li><a href="#letterhead" id="toc-letterhead" class="caption">Letterhead</a></li>
+            <li><a href="#presentation-slides" id="toc-presentation-slides" class="caption">Presentation slides</a></li>
+            </ul>
+            </ul>
+            <li><a href="#product-development" id="toc-digital-product-development" class="caption">Product Development</a></li>
+            <ul>
+            <li><a href="#user-stories" id="toc-user-stories" class="caption">User stories</a></li>
+            <li><a href="#user-journey" id="toc-user-journey" class="caption">User journey</a></li>
+            <li><a href="#user-flow" id="toc-user-flow" class="caption">User flow</a></li>
+            <li><a href="#information-architecture" id="toc-information-architecture" class="caption">Information architecture</a></li>
+            <li><a href="#content-creation" id="toc-content-creation" class="caption">Content creation</a></li>
+            <li><a href="#wireframing" id="toc-wireframing" class="caption">Wireframing</a></li>
+            <li><a href="#design-comps" id="toc-design-comps" class="caption">Design comps</a></li>
+            <li><a href="#prototyping" id="toc-prototyping" class="caption">Prototyping</a></li>
+            <li><a href="#testing" id="toc-testing" class="caption">Usability Testing</a></li>
+            <li><a href="#programming" id="toc-programming" class="caption">Programming</a></li>
+            </ul>
+            <li class="future-section" title="under development"><a href="#brand-activation" id="toc-brand-activation" class="caption">[Brand Activation]</a></li>
+            <li class="future-section" title="under development"><a href="#brand-iteration" id="toc-brand-iteration" class="caption">[Brand Iteration]</a></li>
+            <li><a href="#bibliography" id="toc-bibliography" class="caption">Bibliography</a></li>
+            <ul>
+            <li><a href="#books" id="toc-books" class="caption">Books</a></li>
+            <li><a href="#blogs-and-sources" id="toc-blogs-and-sources" class="caption">Blog &amp; Sources</a></li>
+            <li><a href="#tools-and-resources" id="toc-tools-and-resources" class="caption">Tools &amp; Resources</a></li>
+            </ul>
+            <li><a href="#colophon" id="toc-colophon" class="caption">Colophon</a></li>
+            </ul>
+        </aside>
+
+        <main>
+
+        <h1 class="title-1">Branding Guide</h1>
+        <p>Poly Atlas features a step-by-step guide to the Branding process including practical tips, case studies, template files, and many other useful resources.</p>
 
         <hr>
 
+        <?php include 'content/getting-started.php';?>
+        <hr>
+        <?php include 'content/intro-to-branding.php';?>
+        <hr>
+        <?php include 'content/business-development.php';?>
+        <hr>
+        <?php include 'content/brand-identity-design.php';?>
+        <hr>
+        <?php include 'content/product-development.php';?>
+        <hr>
+        <?php include 'content/bibliography.php';?>
+        <hr>
+        <?php include 'content/colophon.php';?>
+        <hr>
 
-        </div>
-      </div>
+        </main>
     </div>
-
-    <!-- INCLUDE FOOTER.PHP start -->
-      <?php include 'sections/footer.php';?>
-    <!-- INCLUDE FOOTER.PHP end -->
 
   </body>
 </html>
